8dd10057e8e4052d5c99a62860ab4b0c4fcd8ac6
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-07-27 09:34:56 -0700

    Fixes

--- database/config.js
+++ database/config.js
@@ -1,15 +1,17 @@
-// module.exports = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'Courtcowie22',
-//   database: 'weather'
-// };
-
 module.exports = {
-  host: 'us-cdbr-iron-east-02.cleardb.net',
-  user: 'b7cf609352ea53',
-  password: '83301934',
-  database: 'heroku_f32124548e30fc0'
+  host: 'localhost',
+  user: 'root',
+  password: 'Courtcowie22',
+  database: 'weather'
 };
 
+// module.exports = {
+//   host: 'us-cdbr-iron-east-02.cleardb.net',
+//   user: 'b7cf609352ea53',
+//   password: '83301934',
+//   database: 'heroku_f32124548e30fc0'
+// };
+
 //mysql://b7cf609352ea53:83301934@us-cdbr-iron-east-02.cleardb.net/heroku_f32124548e30fc0?reconnect=true
+
+//SHA256:NqBgP5aHjFq7aduT+jAO4BRLoEkTMZHNSpiW7wNnztM rjcowie1@gmail.com
