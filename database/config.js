2bbb7bb591a0e5a32e4e90739f351b4101f3ffc2
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-07-27 10:46:06 -0700

    Fixes

--- database/config.js
+++ database/config.js
@@ -1,16 +1,16 @@
-// module.exports = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'Courtcowie22',
-//   database: 'weather'
-// };
-
 module.exports = {
-  host: 'us-cdbr-iron-east-02.cleardb.net',
-  user: 'b7cf609352ea53',
-  password: '83301934',
-  database: 'heroku_f32124548e30fc0'
+  host: 'localhost',
+  user: 'root',
+  password: 'Courtcowie22',
+  database: 'weather'
 };
+
+// module.exports = {
+//   host: 'us-cdbr-iron-east-02.cleardb.net',
+//   user: 'b7cf609352ea53',
+//   password: '83301934',
+//   database: 'heroku_f32124548e30fc0'
+// };
 
 //test
 
