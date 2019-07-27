3a3cc53e38f5df7b1aaa54a87a23c7349738e949
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-07-27 12:48:35 -0700

    test

--- database/config.js
+++ database/config.js
@@ -1,16 +1,16 @@
-module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: 'Courtcowie22',
-  database: 'weather'
-};
-
 // module.exports = {
-//   host: 'us-cdbr-iron-east-02.cleardb.net',
-//   user: 'b7cf609352ea53',
-//   password: '83301934',
-//   database: 'heroku_f32124548e30fc0'
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'Courtcowie22',
+//   database: 'weather'
 // };
+
+module.exports = {
+  host: 'us-cdbr-iron-east-02.cleardb.net',
+  user: 'b8397969cc5e06',
+  password: '969fc074',
+  database: 'heroku_67c5b230635acbd'
+};
 
 //test test
 
@@ -21,3 +21,7 @@ module.exports = {
 //SHA256:GWjXmXcJARqOcqFIth3UFHhFCn7ZeUoQhpsaTJQIDEY ryancowie@Ryans-Pro.hsd1.ca.comcast.net
 
 // vast-brook-43149
+
+// mysql://b8397969cc5e06:969fc074@us-cdbr-iron-east-02.cleardb.net/heroku_67c5b230635acbd?reconnect=true
+
+// mysql --host=us-cdbr-east.cleardb.com --user=b8397969cc5e06 --password=969fc074 --reconnect heroku_67c5b230635acbd < schema.sql
