6b6958a7479266b23e03f1771978f8b4e5e2ba1f
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-07-27 11:22:40 -0700

    test

--- database/config.js
+++ database/config.js
@@ -12,8 +12,10 @@ module.exports = {
   database: 'heroku_f32124548e30fc0'
 };
 
-//test
+//test test
 
 //mysql://b7cf609352ea53:83301934@us-cdbr-iron-east-02.cleardb.net/heroku_f32124548e30fc0?reconnect=true
 
 //SHA256:NqBgP5aHjFq7aduT+jAO4BRLoEkTMZHNSpiW7wNnztM rjcowie1@gmail.com
+
+//SHA256:GWjXmXcJARqOcqFIth3UFHhFCn7ZeUoQhpsaTJQIDEY ryancowie@Ryans-Pro.hsd1.ca.comcast.net
