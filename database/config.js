18122182f552f1ca7aa32a2778e2913122836036
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-07-27 12:12:23 -0700

    test

--- database/config.js
+++ database/config.js
@@ -1,16 +1,16 @@
-// module.exports = {
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'Courtcowie22',
-//   database: 'weather'
-// };
-
 module.exports = {
-  host: 'us-cdbr-iron-east-02.cleardb.net',
-  user: 'b7cf609352ea53',
-  password: '83301934',
-  database: 'heroku_f32124548e30fc0'
+  host: 'localhost',
+  user: 'root',
+  password: 'Courtcowie22',
+  database: 'weather'
 };
+
+// module.exports = {
+//   host: 'us-cdbr-iron-east-02.cleardb.net',
+//   user: 'b7cf609352ea53',
+//   password: '83301934',
+//   database: 'heroku_f32124548e30fc0'
+// };
 
 //test test
 
@@ -19,3 +19,5 @@ module.exports = {
 //SHA256:NqBgP5aHjFq7aduT+jAO4BRLoEkTMZHNSpiW7wNnztM rjcowie1@gmail.com
 
 //SHA256:GWjXmXcJARqOcqFIth3UFHhFCn7ZeUoQhpsaTJQIDEY ryancowie@Ryans-Pro.hsd1.ca.comcast.net
+
+// vast-brook-43149
